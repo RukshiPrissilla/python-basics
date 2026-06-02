dcfa05c55c11b7e9475f874b51d7901b4cc423df
RukshiPrissilla <rukshiprissilla2006@gmail.com>
2026-06-02 18:13:35 +0530

    Add day2 python learning

---
+++ day2.py
@@ -0,0 +1,36 @@
+
+print("hello")
+
+#VARIABLES
+name="rukshi"#string
+age=56#integer
+height=6.7#float
+is_student=True#boolean
+
+print("my name is",name )
+print("my age is",age)
+print("my age is" + str(age))#here age is string so it can add but in output no space
+
+#IDENTIFY THE TYPE OF VARIABLES
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+#MATHEMATICAL
+a=10
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b) 
+
+#import math is important
+import math
+print(math.sqrt(16))
+print(math.pi)
+print(math.sin(0))
+
