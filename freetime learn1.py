bfdcd5967c735f04fd9dd10f9f04cbfb9c2dd841
RukshiPrissilla <289871842+RukshiPrissilla@users.noreply.github.com>
2026-06-04 19:12:25 +0530

    learn

---
+++ freetime learn1.py
@@ -0,0 +1,17 @@
+ktl=5
+print(ktl)
+print(type(ktl))#here ktl is string bcs i identify it as strin above
+
+name=input("name:")
+print("you are beautiful",name)#comma is important
+
+Name=input("Name:")
+print("i hate u", Name)
+
+i=6
+if i<4:
+    print("i am glad")
+else:
+    print("i am not glad")
+
+
