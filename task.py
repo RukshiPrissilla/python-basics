476389a6998fa9834bbbc4a54483be130e57769d
RukshiPrissilla <289871842+RukshiPrissilla@users.noreply.github.com>
2026-06-15 13:43:50 +0530

    fun with chitty-loops and mathematics

---
+++ task.py
@@ -0,0 +1,22 @@
+#task from chitty
+
+print("Numbers:1 2 3 4 5")
+print()
+
+print("Squares")
+total=0
+for number in range(1,6):
+    Squares=number*number
+    print(f"{number}->{Squares}")
+    
+    total += number
+
+print(f"\nSum={total}")
+average=total/5
+print(f"average={average}")
+    
+    
+
+
+
+    
