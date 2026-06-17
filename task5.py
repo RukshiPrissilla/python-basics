fc67c90fa74386d766a7d7c9436de4511466d53e
Rukshi Prissilla <289871842+RukshiPrissilla@users.noreply.github.com>
2026-06-17 06:38:28 +0530

    using loops & def

---
+++ task5.py
@@ -0,0 +1,23 @@
+#task like ask for num if that num plus it gives the square if tht num minus it skips tht num if it 0 the program should be stoped
+
+def Square(a):
+    return a**2
+
+#while True:
+    a=int(input("Enter a number(0 to stop):"))
+    if a==0:
+        print("Program ended")
+        #break #stops here 
+        
+    if a<0:
+        print("Negative numbers are skipped")
+        
+    if a>0:
+        print(f"Square={Square(a)}")
+
+for i in range(1,6):
+    for y in range(1,i+1):
+        print(y,end=" ")
+    print()
+        
+    
